386851b3943ae1dce9ef055b63ab3e8026eed48f
Dmitry <fcinterna@yandex.ru>
2020-04-29 17:31:32 +0300

    Немного изменился gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,9 +3,7 @@ const del = require('del');
 const less = require('gulp-less');
 const fileinclude = require('gulp-file-include');
 const pug = require('gulp-pug');
-const newer = require('gulp-newer');
 const changed = require('gulp-changed');
-const cache = require('gulp-cache');
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
@@ -13,37 +11,12 @@ const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const browserSync = require('browser-sync').create();
 
-// ============= C ФРЕЙМВОРКОМ UIKIT =============
-function uikitStyles() {
-  return src('node_modules/uikit/src/less/components/**')
-    .pipe(dest('_src/less/uikit'));
-}
-
-function uikitJs() {
-  return src(['node_modules/uikit/dist/js/uikit-icons.min.js', 'node_modules/uikit/dist/js/uikit.min.js'])
-    .pipe(dest('_src/js'));
-}
-
-// ============= БЕЗ ФРЕЙМВОРКА =============
-function jquery() {
-  return src('node_modules/jquery/dist/jquery.min.js')
-    .pipe(dest('_src/js'));
-}
-
-function normalize() {
-  return src('node_modules/normalize.css/normalize.css')
-    .pipe(rename('normalize.less'))
-    .pipe(dest('_src/less'));
-}
-// ============= /.БЕЗ ФРЕЙМВОРКА =============
-
+// ============= УДАЛЯЕТ ПАПКУ BUILD =============
 function clean() {
   return del(['build']);
 }
 
-// Используй простое включение html в html
-// актуально для быстрой разработки на uikit
-// или используй pug, расскомментировав задачу ниже
+// ============= HTML таск (gulp-file-include) =============
 function html() {
   return src('_src/html/index.html')
     .pipe(fileinclude({
@@ -55,60 +28,76 @@ function html() {
     .pipe(browserSync.stream());
 }
 
+// ============= PUG =============
 function pugWay() {
-  return src(['_src/pug/**/*.pug', '!_src/pug/includes/**', '!_src/pug/templates/**'])
-    .pipe(pug({ pretty: true })) // чтобы не сжимался на выходе
-    .pipe(dest('build'))
-    .pipe(browserSync.stream());
+ return src(['_src/pug/**/*.pug', '!_src/pug/includes/**', '!_src/pug/templates/**'])
+   .pipe(pug({ pretty: true })) // чтобы не сжимался на выходе
+   .pipe(dest('build'))
+   .pipe(browserSync.stream());
 }
 
-function styles() {
-  return src('_src/less/styles.less')
-    // .pipe(newer('build/css'))
-    .pipe(sourcemaps.init())
-    .pipe(less())
-    // .pipe(cache(less())) //- есть проблема с sourcemaps для less
-    // .pipe(cache(less(), { fileCache: new cache.Cache({ cacheDirName: 'gulp-cache' })}))
-    .pipe(autoprefixer({ overrideBrowserslist: ['last 5 versions'], cascade: false }))
-    .pipe(sourcemaps.write('./'))
-    .pipe(dest('build/css'))
-    .pipe(browserSync.stream());
-}
-
+// ============= СКРИПТЫ =============
 function scripts() {
-  // Если без фремворка, не забыть заменить скрипты uikit на jquery _src/js/jquery.min.js
-  return src(['_src/js/uikit-icons.min.js', '_src/js/uikit.min.js', '_src/js/custom.js'])
+  // return src(['_src/js/uikit-icons.min.js', '_src/js/uikit.min.js', '_src/js/custom.js']) // Расскоментировать если нужен UIKIT
+  return src(['_src/js/custom.js']) // Закоментировать, если расскоментирована строчка выше
     .pipe(concat('scripts.js'))
     .pipe(uglify())
     .pipe(dest('build/js'))
     .pipe(browserSync.stream());
 }
 
+// ============= СТИЛИ =============
+function styles() {
+  return src('_src/less/styles.less')
+    .pipe(sourcemaps.init())
+    .pipe(less())
+    .pipe(autoprefixer({ overrideBrowserslist: ['last 5 versions'], cascade: false }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(dest('build/css'))
+    .pipe(browserSync.stream());
+}
+
+// ============= КОПИРУЕТ ФАЙЛЫ И ПАПКИ ИЗ SRC В BUILD =============
 function copyFiles() {
-  // base требуется для корректного переноса файлов и папок
-  // в данном пример для fonts (чтобы шрифты после переноса также находились в папке fonts)
-  return src(['_src/fonts/**', '_src/*.ico', '_src/robots.txt'], { 'base': '_src' })
-    // .pipe(newer('build'))
+  return src(['_src/img/**', '_src/fonts/**', '_src/*.ico', '_src/robots.txt'], { 'base': '_src' }) // base треб. для кор-го переноса файлов и папок (для fonts)
     .pipe(dest('build'));
 }
 
+// ============= СЛЕДИТ ЗА ИЗМЕНЕНИЯМИ В ФАЙЛАХ И ПАПКАХ И РЕФРЕШИТ БРАУЗЕР =============
 function watching() {
   browserSync.init({
     server: {
-      baseDir: './build'
-    }
+      baseDir: './build'  // сервер запускается в этой директории
+    },
+    browser: ["chrome"]
   });
   watch('_src/js/**/*.js', scripts); // следит за скриптами в _src и запускает задачу scripts
   watch('_src/less/**/*.less', styles); // следит за стилями
   watch('_src/html/**/*.html', html); // следит за html
   watch('_src/pug/**/*.pug', pugWay); // следит за pug
-  watch(['_src/fonts/**', '_src/*.ico', '_src/robots.txt'], copyFiles); // следит за файлами
+  watch(['_src/img/**', '_src/fonts/**', '_src/*.ico', '_src/robots.txt'], copyFiles); // следит за файлами
   watch('build/*.html').on('change', browserSync.reload); // перегружает браузер, если файлы html в build изменились
 }
 
-exports.clean = clean;
-// Если без фреймворка = gulp simple
-exports.simple = parallel(normalize, jquery);
-// Если с фреймворком = gulp uikit (по-умолчанию)
-exports.uikit = parallel(uikitStyles, uikitJs);
-exports.default = series(clean, parallel(pugWay, styles, scripts, copyFiles), watching);
+// ============= C ФРЕЙМВОРКОМ UIKIT настроить шаги: =============
+// 1. Выполнить таск gulp uikit (скопируются стили и скрипты фреймворка в папку src)
+// 2. В таске скрипты расскоментировать return src(['_src/js/uikit-icons.min.js', '_src/js/uikit.min.js', '_src/js/custom.js'])
+// 3. В таске скрипты закоментировать return src(['_src/js/custom.js'])
+// 4. в index.html и layout.pug закоментировать normalize и jquery
+// 5. В styles.less расскоментировать @import "uikit/_import.less";
+// 6. Если нужен pug вместо html, то заменить  exports.default = series(clean, parallel(pugWay, styles, scripts, copyFiles), watching);
+
+function uikitStyles() {
+  return src('node_modules/uikit/src/less/components/**')
+    .pipe(dest('_src/less/uikit'));
+}
+
+function uikitJs() {
+  return src(['node_modules/uikit/dist/js/uikit-icons.min.js', 'node_modules/uikit/dist/js/uikit.min.js'])
+    .pipe(dest('_src/js'));
+}
+
+//************* ЗАПУСК ТАСКОВ *************
+exports.clean = clean; // gulp clean
+exports.uikit = parallel(uikitStyles, uikitJs); // gulp uikit
+exports.default = series(clean, parallel(html, styles, scripts, copyFiles), watching); // gulp (по-умолчанию: html + normalize cdn + jquery cdn)
